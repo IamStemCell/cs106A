4cc4deb5f98bb5a57125fc2ef6fd031e0c7993ad
IamStemCell <113768157+IamStemCell@users.noreply.github.com>
2024-05-12 21:40:01 +0300

    Create Mod3.py

---
+++ Mod3.py
@@ -0,0 +1,39 @@
+import turtle
+
+def koch_curve(t, order, size):
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_curve(t, order-1, size/3)
+            t.left(angle)
+
+def koch_snowflake(t, order, size):
+    for _ in range(3):
+        koch_curve(t, order, size)
+        t.right(120)
+    t.hideturtle()
+
+def main():
+   
+    screen = turtle.Screen()
+    screen.setup(width=900, height=900)
+    screen.title("Koch Snowflake for Code In Place 2024 by Svitlana P")
+    screen.bgcolor("black")
+
+    t = turtle.Turtle()
+    t.speed(0)
+    t.color("white")
+    t.hideturtle()
+    
+
+    t.penup()
+    t.goto(-150, 100)
+    t.pendown()
+    koch_snowflake(t, 4, 300)
+
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
+  
