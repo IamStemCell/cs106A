e53ca62910a01c9716677a28a43a8a2217b2ac29
IamStemCell <113768157+IamStemCell@users.noreply.github.com>
2024-05-12 21:53:01 +0300

    Update Mod3.py

--- Mod3.py
+++ Mod3.py
@@ -1,5 +1,24 @@
 import turtle
 
+def main():
+   
+    screen = turtle.Screen()
+    screen.setup(width=900, height=900)
+    screen.title("Koch Snowflake for Code In Place 2024 by Svitlana P")
+    screen.bgcolor("black")
+
+    t = turtle.Turtle()
+    t.speed(0)
+    t.color("white")
+    t.hideturtle()
+    
+    t.penup()
+    t.goto(-150, 100)
+    t.pendown()
+    koch_snowflake(t, 4, 300)
+
+    screen.mainloop()
+
 def koch_curve(t, order, size):
     if order == 0:
         t.forward(size)
@@ -13,26 +32,6 @@ def koch_snowflake(t, order, size):
         koch_curve(t, order, size)
         t.right(120)
     t.hideturtle()
-
-def main():
-   
-    screen = turtle.Screen()
-    screen.setup(width=900, height=900)
-    screen.title("Koch Snowflake for Code In Place 2024 by Svitlana P")
-    screen.bgcolor("black")
-
-    t = turtle.Turtle()
-    t.speed(0)
-    t.color("white")
-    t.hideturtle()
-    
-
-    t.penup()
-    t.goto(-150, 100)
-    t.pendown()
-    koch_snowflake(t, 4, 300)
-
-    screen.mainloop()
 
 if __name__ == "__main__":
     main()
